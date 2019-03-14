4c6285a40b82da58f462e279cc23c3a28a6e985d
Gowtham Behara <gothsiddu@gmail.com>
2019-03-14 12:07:26 +0530

    Create Email Application

---
+++ src/main/java/EmailApp.java
@@ -0,0 +1,16 @@
+import com.yodlee.springdependencyinjection.configuration.EmailConfiguration;
+import com.yodlee.springdependencyinjection.consumer.ConsumerWithAnnotation;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class EmailApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(EmailConfiguration.class);
+        ConsumerWithAnnotation application = context.getBean(ConsumerWithAnnotation.class);
+
+        String message = "Spring Framwork";
+        String recepient = "gothsiddu@gmail.com";
+        application.processMessage(message, recepient);
+
+        context.close();
+    }
+}
